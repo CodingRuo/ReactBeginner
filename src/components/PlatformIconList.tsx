5b9991346d95d6e9b86116cd8fab1050aebacf49
codingruo <r.schober@outlook.com>
2025-04-17 07:51:29 +0200

    Get optimized images

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,17 @@
 import { Platform } from "@/hooks/useGames";
 import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
 import {
-  FaWindows,
-  FaPlaystation,
-  FaXbox,
-  FaLinux,
   FaAndroid,
   FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
-import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
@@ -33,7 +33,11 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color={"gray.500"} />
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+        />
       ))}
     </HStack>
   );
