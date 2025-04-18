5b0b20abcf91d0a846d6b47365d8a4fdbb872219
codingruo <r.schober@outlook.com>
2025-04-18 09:18:57 +0200

    Push

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 import { useState } from "react";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
 import { Platform } from "./hooks/useGames";
 import { Genre } from "./hooks/useGenres";
-import GameHeading from "./components/GameHeading";
+import PostList from "./react-query/PostList";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -20,52 +14,56 @@ function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "200px 1fr",
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar
-          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
-        />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
-          <GenreList
-            selectedGenre={gameQuery.genre}
-            onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
-          />
-        </GridItem>
-      </Show>
-      <GridItem area="main">
-        <Box paddingLeft={2}>
-          <GameHeading gameQuery={gameQuery} />
-          <Flex marginBottom={5}>
-            <Box marginRight={5}>
-              <PlatformSelector
-                selectedPlatform={gameQuery.platform}
-                onSelectedPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })
-                }
-              />
-            </Box>
-            <SortSelector
-              sortOrder={gameQuery.sortOrder}
-              onSelectSortOrder={(sortOrder) =>
-                setGameQuery({ ...gameQuery, sortOrder })
-              }
-            />
-          </Flex>
-        </Box>
-        <GameGrid gameQuery={gameQuery} />
-      </GridItem>
-    </Grid>
+    <>
+      <PostList />
+      {/* <TodoList /> */}
+    </>
+    // <Grid
+    //   templateAreas={{
+    //     base: `"nav" "main"`,
+    //     lg: `"nav nav" "aside main"`,
+    //   }}
+    //   templateColumns={{
+    //     base: "1fr",
+    //     lg: "200px 1fr",
+    //   }}
+    // >
+    //   <GridItem area="nav">
+    //     <NavBar
+    //       onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+    //     />
+    //   </GridItem>
+    //   <Show above="lg">
+    //     <GridItem area="aside" paddingX={5}>
+    //       <GenreList
+    //         selectedGenre={gameQuery.genre}
+    //         onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+    //       />
+    //     </GridItem>
+    //   </Show>
+    //   <GridItem area="main">
+    //     <Box paddingLeft={2}>
+    //       <GameHeading gameQuery={gameQuery} />
+    //       <Flex marginBottom={5}>
+    //         <Box marginRight={5}>
+    //           <PlatformSelector
+    //             selectedPlatform={gameQuery.platform}
+    //             onSelectedPlatform={(platform) =>
+    //               setGameQuery({ ...gameQuery, platform })
+    //             }
+    //           />
+    //         </Box>
+    //         <SortSelector
+    //           sortOrder={gameQuery.sortOrder}
+    //           onSelectSortOrder={(sortOrder) =>
+    //             setGameQuery({ ...gameQuery, sortOrder })
+    //           }
+    //         />
+    //       </Flex>
+    //     </Box>
+    //     <GameGrid gameQuery={gameQuery} />
+    //   </GridItem>
+    // </Grid>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "bootstrap/dist/css/bootstrap.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme.ts";
+import "./index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
+  // <StrictMode>
+  //   {/* <ChakraProvider theme={theme}> */}
+  //   {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
+  //   <App />
+  //   {/* </ChakraProvider> */}
+  // </StrictMode>
+
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <QueryClientProvider client={queryClient}>
       <App />
-    </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );
